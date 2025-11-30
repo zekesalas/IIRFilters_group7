baddeab840765341897d48f21b2126d902b75e34
Zeke <u0882690@umail.utah.edu>
2025-11-29 18:45:28 -0700

    Finished 2.4f

--- ECGInterferenceRemoval/Lab_Exercise/Verification_Page.m
+++ ECGInterferenceRemoval/Lab_Exercise/Verification_Page.m
@@ -41,6 +41,7 @@ t = (0:length(ecgsig)-1)/fs; %time index
 t1 = 0;   t2 = 5;
 i = (t >= t1) & (t <= t2);
 
+figure(1)
 subplot(2,1,1);
 plot(t(i), ecgsig(i));
 xlabel('Time (s)');
@@ -52,3 +53,42 @@ plot(t(i), y(i));
 xlabel('Time (s)');
 ylabel('Amplitude');
 title('ECG after notch filter');
+
+
+% 2.4f
+
+Fs = 750;                 % sampling frequency (Hz)
+
+% FFT of original and filtered signals
+ECGFFT = fft(ecgsig);
+YFFT   = fft(y);
+
+N = length(ECGFFT);       % assume ecgsig and y have same length
+
+% Shift zero frequency to the center (for -Fs/2 ... Fs/2 view)
+ECGFFT = fftshift(ECGFFT);
+YFFT   = fftshift(YFFT);
+
+% Frequency axis in Hz (corresponding to fftshifted data)
+f = (-N/2:N/2-1) * (Fs/N);   % goes from -Fs/2 to Fs/2
+
+figure(2);
+subplot(2,1,1);
+plot(f, abs(ECGFFT));
+xlabel('Frequency (Hz)');
+ylabel('|X(f)|');
+title('Original ECG Spectrum');
+grid on;
+
+subplot(2,1,2);
+plot(f, abs(YFFT));
+xlabel('Frequency (Hz)');
+ylabel('|Y(f)|');
+title('Filtered ECG Spectrum');
+grid on;
+
+% The magnitude spectrum of the original ECG (top plot) shows a narrow peak near 59.4 Hz,
+% corresponding to the sinusoidal interference. In the filtered ECG spectrum (bottom plot),
+% this peak is greatly reduced while the rest of the spectrum remains largely unchanged,
+% indicating that the notch filter has effectively removed the 59.4 Hz interference without
+% significantly altering the underlying signal.
